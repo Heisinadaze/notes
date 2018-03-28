5c6794e01f2deb63e32e7fe65d7e8a2f38713f2b
heisinsdaze <m18435170294@163.com>
2018-03-28 17:43:44 +0800

    按钮

---
+++ ts/index.ts
@@ -0,0 +1,25 @@
+
+let i: number = 1
+
+let text: string = 'aa'
+
+let isTrue: boolean = true
+
+let arr: Array<any> = []
+
+let obj: {x: number, y: string} = {x: 1, y: '1'}
+
+let fn = (arr: string[], callback: (ind: number) => string[] ) => {
+  return arr
+}
+
+/**
+ * public 公开的，可以暴露的方法
+ * private 私有的 不能在外部访问
+ * protected 受保护的，可以在派生类中访问
+ */
+
+ class Animal {
+   
+ }
+
