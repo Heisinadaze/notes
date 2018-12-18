a4eec36cdbad6a2e49f83f4871ce3bafe34cc0f3
heisinsdaze <m18435170294@163.com>
2018-12-18 14:21:27 +0800

    为你我受冷风吹

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.9669d1fa90c800fbedf1cf51bfe353d3.js"
+  "precache-manifest.aa475a57deb866b3297d6e55da3b21b7.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "deteative"});
